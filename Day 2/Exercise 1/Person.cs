7e5c5cf3f757822d12e10f702fe262c0aee3691c
JustinLaffer <justinlaffer155@gmail.com>
2018-05-30 20:20:05 -0700

    updates

---
+++ Day 2/Exercise 1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ZombieSimulator
+{
+  public class Person
+    {  
+        public int DistanceTraveled{get;set;}
+        
+        public Person()
+        {
+         Console.WriteLine("new person created");
+        }
+      public void Walk(int minutes)
+        {
+            DistanceTraveled = DistanceTraveled + minutes;
+        }
+        public int CalculateDistance(int minutes)
+        {
+          return minutes*2;
+        }
+    }
+}
+
---
+++ Day 2/Exercise 1/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ZombieSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Justin= new Person();
+        }
+    }
+}
