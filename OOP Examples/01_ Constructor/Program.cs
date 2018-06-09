97b6aa185383c92d950f0907bec32aa27df80e1f
JustinLaffer <justinlaffer155@gmail.com>
2018-06-09 12:38:21 -0700

    updates

---
+++ OOP Examples/01_ Constructor/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TimeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeMachine machine = new TimeMachine();
+        }
+    }
+}
